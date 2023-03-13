9b2dad5a0823dd670fd0cf1a185e0c015f2516e2
RivkyShachar <rvider@g.jct.ac.il>
2023-03-13 09:04:59 +0200

    Add project files.

---
+++ OnlinePizzaWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlinePizzaWebApplication.Data;
+using OnlinePizzaWebApplication.Repositories;
+
+namespace OnlinePizzaWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Pizzas
+        private readonly AppDbContext _context;
+        private readonly IPizzaRepository _pizzaRepo;
+        private readonly ICategoryRepository _categoryRepo;
+
+        public HomeController(AppDbContext context, IPizzaRepository pizzaRepo, ICategoryRepository categoryRepo)
+        {
+            _context = context;
+            _pizzaRepo = pizzaRepo;
+            _categoryRepo = categoryRepo;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _pizzaRepo.GetAllIncludedAsync());
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
