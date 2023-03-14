8c62ff6cf842e68de6a14ea584be224acd9df323
RivkyShachar <rvider@g.jct.ac.il>
2023-03-14 15:49:07 +0200

    sorting categories try

--- OnlinePizzaWebApplication/Startup.cs
+++ OnlinePizzaWebApplication/Startup.cs
@@ -118,6 +118,7 @@ namespace OnlinePizzaWebApplication
                 endpoints.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+
             });
         }
     }
