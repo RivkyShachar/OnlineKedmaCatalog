48f0199ae4f61ad86277a2d5a82bcc8a67e50fa0
RivkyShachar <rvider@g.jct.ac.il>
2023-03-14 14:50:42 +0200

    added the categories menu in index from database

---
+++ OnlinePizzaWebApplication/ViewModels/PizzasCaregoriesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using OnlinePizzaWebApplication.Models;
+using System.Collections.Generic;
+
+namespace OnlineKedmaWebApplication.ViewModels
+{
+    public class PizzasCaregoriesViewModel
+    {
+        public IEnumerable<Pizzas> Pizzas { get; set; }
+        public IEnumerable<Categories> Categories { get; set; }
+    }
+}
+
+
