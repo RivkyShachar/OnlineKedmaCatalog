c68356af0b9335bbb8dfca79396fed9cceaf2cc4
Yael <yaelgloiberman@gmail.com>
2023-03-14 12:39:21 +0200

    h

--- OnlinePizzaWebApplication/Controllers/ShoppingCartController.cs
+++ OnlinePizzaWebApplication/Controllers/ShoppingCartController.cs
@@ -77,6 +77,57 @@ namespace OnlinePizzaWebApplication.Controllers
 
             return RedirectToAction("Index");
         }
+      
+      
+        public async Task<IActionResult> SendToPdf(string emailAddress)
+        {
+            // Retrieve the items in the shopping cart
+            List<CartItem> cartItems = await _shoppingCart.GetShoppingCartItemsAsync();
 
+            // Create a new PDF document
+            var document = new Document();
+            var writer = PdfWriter.GetInstance(document, new MemoryStream());
+            document.Open();
+
+            // Add the shopping cart items to the PDF
+            foreach (var item in cartItems)
+            {
+                document.Add(new Paragraph(item.Name));
+                document.Add(new Paragraph(item.Barcode));
+                // Add more details as needed
+            }
+
+            // Close the PDF document
+            document.Close();
+
+            // Create a new email message
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Your Name", "your-email@example.com"));
+            message.To.Add(new MailboxAddress("", emailAddress));
+            message.Subject = "Shopping Cart PDF";
+
+            // Create a new MIME part for the PDF attachment
+            var attachment = new MimePart("application", "pdf")
+            {
+                Content = new MimeContent(new MemoryStream(writer.DirectContentUnder.RawContent), ContentEncoding.Default),
+                ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+                FileName = "shopping_cart.pdf"
+            };
+
+            // Add the PDF attachment to the email message
+            var multipart = new Multipart("mixed");
+            multipart.Add(attachment);
+            message.Body = multipart;
+
+            // Connect to the SMTP server and send the email
+            using var client = new SmtpClient();
+            await client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+            await client.AuthenticateAsync("your-email@example.com", "your-email-password");
+            await client.SendAsync(message);
+            await client.DisconnectAsync(true);
+
+            // Return a success message to the user
+            return Content("PDF sent successfully!");
+        }
     }
 }
