afd7c03dfc899a16cde29b0498897497e77b839d
RivkyShachar <rvider@g.jct.ac.il>
2023-03-19 11:37:34 +0200

    basic send email is working!!!!!!!!

--- OnlinePizzaWebApplication/Controllers/ShoppingCartController.cs
+++ OnlinePizzaWebApplication/Controllers/ShoppingCartController.cs
@@ -22,8 +22,7 @@ using Microsoft.Extensions.Primitives;
 using System.Reflection;
 using Microsoft.CodeAnalysis;
 using System.Text.RegularExpressions;
-
-
+using MimeKit;
 
 namespace OnlinePizzaWebApplication.Controllers
 {
@@ -205,7 +204,7 @@ namespace OnlinePizzaWebApplication.Controllers
             Document Doc = new Document(PageSize.LETTER);
 
             //Create our file stream
-            using (FileStream fs = new FileStream(@"C:\\Users\\yaeli\\Desktop\\h.pdf", FileMode.Create, FileAccess.Write, FileShare.Read))
+            using (FileStream fs = new FileStream(@"C:\Users\rivky\OneDrive\היה פה\שולחן העבודה\h.pdf", FileMode.Create, FileAccess.Write, FileShare.Read))
             {
 
 
@@ -279,15 +278,314 @@ namespace OnlinePizzaWebApplication.Controllers
 
         }
 
+        [HttpPost]
+        public async Task<IActionResult> GenerateAndSendPdf()
+        {
+            var shoppingCartItems = await _shoppingCart.GetShoppingCartItemsAsync();
+
+            Document Doc = new Document(PageSize.LETTER);
+
+            //Create our file stream
+            using (var memoryStream = new MemoryStream())
+            {
+                // Create a new PDF writer
+                PdfWriter writer = PdfWriter.GetInstance(Doc, memoryStream);
+                // Set the page size to A4
+                Doc.SetPageSize(PageSize.A4);
+                // Set the margins for the document
+                Doc.SetMargins(36f, 36f, 36f, 36f);
+                // Open the PDF document
+                Doc.Open();
+
+                var currentDate = DateTime.Now;
+                // Get the customer name and shipping ID from your data source
+                var customerName = "Yaeli Gloiberman";
+                var shippingId = "213245632";
+                // Add the date, customer name, and shipping ID to the PDF document
+                var paragraph = new Paragraph($"Date: {currentDate.ToShortDateString()}    Customer: {customerName}    Shipping ID: {shippingId}");
+                paragraph.SpacingAfter = 20f;
+                Doc.Add(paragraph);
+
+                //Full path to the Arial file
+                string ARIALUNI_TFF = System.IO.Path.Combine(@"C:\\Windows\\Fonts", "arial.ttf");
+
+                //Create a base font object making sure to specify IDENTITY-H
+                BaseFont bf = BaseFont.CreateFont(ARIALUNI_TFF, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+
+                //Create a specific font object
+                iTextSharp.text.Font f = new iTextSharp.text.Font(bf, 14);
+
+                //Use a table so that we can set the text direction
+                PdfPTable T = new PdfPTable(5);
+                T.WidthPercentage = 90;
+                T.DefaultCell.BorderWidth = 1;
+                T.RunDirection = PdfWriter.RUN_DIRECTION_RTL;
+                T.AddCell(new Phrase("שם מוצר", f));
+                T.AddCell(new Phrase("ברקוד", f));
+                T.AddCell(new Phrase("כמות בארגז", f));
+                T.AddCell(new Phrase("כמות ביחידים", f));
+                T.AddCell(new Phrase("מחיר", f));
+
+                foreach (var item in shoppingCartItems)
+                {
+                    T.AddCell(new Phrase(item.Pizza.Name, f));
+                    T.AddCell(new Phrase(item.Pizza.Barcode, f));
+                    T.AddCell(new Phrase(item.AmountBoxes.ToString(), f));
+                    T.AddCell(new Phrase(item.AmountSingles.ToString(), f));
+                    T.AddCell(new Phrase(item.Pizza.Price.ToString(), f));
+                }
+                Doc.Add(T);
+
+                if (writer.GetVerticalPosition(false) < Doc.BottomMargin)
+                {
+                    // If the table extends beyond the current page, add a new page
+                    Doc.NewPage();
+                    // Add the date, customer name, and shipping ID to the new page
+                    Doc.Add(paragraph);
+                    // Add the table to the new page
+                    Doc.Add(T);
+                }
+                // Close the PDF document
+                Doc.Close();
 
 
-     
+                var pdfBytes = memoryStream.ToArray();
+
+                // Create a System.Net.Mail.MailMessage object
+                MailMessage message = new MailMessage();
+                //message.From = new MailAddress("rvider@g.jct.ac.il");
+                // Add a recipient
+                message.To.Add("rvider@g.jct.ac.il");
+
+                // Add a message subject
+                message.Subject = "Account activation";
+
+                // Add a message body
+                message.Body = "Hello customer" + "\n" +
+                    "Welcome to Kedma.\n" +
+                    "We are so happy you have chosen to join us  and  are sure you will enjoy our services.\n" +
+                    "For any question, problems or requests you can contact our customer service team " +
+                    "at customerService@DroneDrop.com and they will gladly assist you.\n\n" +
+                    "Thank you and have a great day,\n" +
+                    "Team DroneDrop";
+
+                // Create a System.Net.Mail.MailAddress object and 
+                // set the sender email address and display name.
+                message.From = new MailAddress("rvider@g.jct.ac.il", "Kedma");
+
+                // Create a MemoryStream object from your PDF file
+                MemoryStream memoryStream1 = new MemoryStream(pdfBytes);
+
+                // Create a new attachment with the PDF bytes
+                Attachment attachment = new Attachment(memoryStream1, "shopping_cart.pdf", "application/pdf");
+
+                // Add the attachment to the email message
+                message.Attachments.Add(attachment);
+
+                //// Create a new attachment with the PDF bytes
+                //var attachment = new MimePart("application", "pdf")
+                //{
+                //    Content = new MimeContent(memoryStream),
+                //    ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+                //    ContentTransferEncoding = ContentEncoding.Default,
+                //    FileName = "shopping_cart.pdf"
+                //};
+
+                //// Add the attachment to the email message
+                //var multipart = new Multipart("mixed");
+                //multipart.Add(attachment);
+                //message.Body = multipart.ToString();
+
+                // Create a System.Net.Mail.SmtpClient object
+                // and set the SMTP host and port number
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+
+                // If your server requires authentication add the below code
+                // =========================================================
+                // Enable Secure Socket Layer (SSL) for connection encryption
+                smtp.EnableSsl = true;
+
+                // Do not send the DefaultCredentials with requests
+                smtp.UseDefaultCredentials = false;
+
+                // Create a System.Net.NetworkCredential object and set
+                // the username and password required by your SMTP account
+                smtp.Credentials = new NetworkCredential("dronedrop2021@gmail.com", "ouiatjczzhkvtxnr");
+                // =========================================================
+
+                // Send the message
+                smtp.Send(message);
+
+
+                // Redirect to the previous page
+                return RedirectToAction("Index");
+            }
+        }
+        //public async Task<IActionResult> GenerateAndSendPdf()
+        //{
+        //    var shoppingCartItems = await _shoppingCart.GetShoppingCartItemsAsync();
+
+        //    Document Doc = new Document(PageSize.LETTER);
+
+        //    //Create our file stream
+        //    using (var memoryStream = new MemoryStream())
+        //    {
+        //        // Create a new PDF writer
+        //        PdfWriter writer = PdfWriter.GetInstance(Doc, memoryStream);
+        //        // Set the page size to A4
+        //        Doc.SetPageSize(PageSize.A4);
+        //        // Set the margins for the document
+        //        Doc.SetMargins(36f, 36f, 36f, 36f);
+        //        // Open the PDF document
+        //        Doc.Open();
+
+        //        var currentDate = DateTime.Now;
+        //        // Get the customer name and shipping ID from your data source
+        //        var customerName = "Yaeli Gloiberman";
+        //        var shippingId = "213245632";
+        //        // Add the date, customer name, and shipping ID to the PDF document
+        //        var paragraph = new Paragraph($"Date: {currentDate.ToShortDateString()}    Customer: {customerName}    Shipping ID: {shippingId}");
+        //        paragraph.SpacingAfter = 20f;
+        //        Doc.Add(paragraph);
+
+        //        //Full path to the Arial file
+        //        string ARIALUNI_TFF = System.IO.Path.Combine(@"C:\\Windows\\Fonts", "arial.ttf");
+
+        //        //Create a base font object making sure to specify IDENTITY-H
+        //        BaseFont bf = BaseFont.CreateFont(ARIALUNI_TFF, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+
+        //        //Create a specific font object
+        //        iTextSharp.text.Font f = new iTextSharp.text.Font(bf, 14);
+
+        //        //Use a table so that we can set the text direction
+        //        PdfPTable T = new PdfPTable(5);
+        //        T.WidthPercentage = 90;
+        //        T.DefaultCell.BorderWidth = 1;
+        //        T.RunDirection = PdfWriter.RUN_DIRECTION_RTL;
+        //        T.AddCell(new Phrase("שם מוצר", f));
+        //        T.AddCell(new Phrase("ברקוד", f));
+        //        T.AddCell(new Phrase("כמות בארגז", f));
+        //        T.AddCell(new Phrase("כמות ביחידים", f));
+        //        T.AddCell(new Phrase("מחיר", f));
+
+        //        foreach (var item in shoppingCartItems)
+        //        {
+        //            T.AddCell(new Phrase(item.Pizza.Name, f));
+        //            T.AddCell(new Phrase(item.Pizza.Barcode, f));
+        //            T.AddCell(new Phrase(item.AmountBoxes.ToString(), f));
+        //            T.AddCell(new Phrase(item.AmountSingles.ToString(), f));
+        //            T.AddCell(new Phrase(item.Pizza.Price.ToString(), f));
+        //        }
+        //        Doc.Add(T);
+
+        //        if (writer.GetVerticalPosition(false) < Doc.BottomMargin)
+        //        {
+        //            // If the table extends beyond the current page, add a new page
+        //            Doc.NewPage();
+        //            // Add the date, customer name, and shipping ID to the new page
+        //            Doc.Add(paragraph);
+        //            // Add the table to the new page
+        //            Doc.Add(T);
+        //        }
+        //        // Close the PDF document
+        //        Doc.Close();
+
+        //        var pdfBytes = memoryStream.ToArray();
+
+        //        var emailMessage = new MimeMessage();
+        //        emailMessage.From.Add(new MailboxAddress("Sender Name", "rvider@g.jct.ac.il"));
+        //        emailMessage.To.Add(new MailboxAddress("Recipient Name", "rvider@g.jct.ac.il"));
+        //        emailMessage.Subject = "Shopping Cart Invoice";
+
+        //        // Create a new attachment with the PDF bytes
+        //        var attachment = new MimePart("application", "pdf")
+        //        {
+        //            Content = new MimeContent(memoryStream),
+        //            ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+        //            ContentTransferEncoding = ContentEncoding.Base64,
+        //            FileName = "shopping_cart.pdf"
+        //        };
+
+        //        // Add the attachment to the email message
+        //        var multipart = new Multipart("mixed");
+        //        multipart.Add(attachment);
+        //        emailMessage.Body = multipart;
+
+        //        // Send the email using the SMTP client
+        //        using (var client = new SmtpClient())
+        //        {
+        //            // Connect to the SMTP server
+        //            await client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+        //            // Authenticate with the SMTP server
+        //            await client.AuthenticateAsync("sender@example.com", "password");
+        //            // Send the email message
+        //            await client.SendAsync(emailMessage);
+        //            // Disconnect from the SMTP server
+        //            await client.DisconnectAsync(true);
+        //        }
+
+        //        return RedirectToAction("Index");
+
+
+        //    }
+        //}
+
+
+        [HttpPost]
+        public IActionResult SendEmail()
+        {
+            // Create a System.Net.Mail.MailMessage object
+            MailMessage message = new MailMessage();
+
+            // Add a recipient
+            message.To.Add("rvider@g.jct.ac.il");
+
+            // Add a message subject
+            message.Subject = "Account activation";
+
+            // Add a message body
+            message.Body = "Hello customer" + "\n" +
+                "Welcome to Kedma.\n" +
+                "We are so happy you have chosen to join us  and  are sure you will enjoy our services.\n" +
+                "For any question, problems or requests you can contact our customer service team " +
+                "at customerService@DroneDrop.com and they will gladly assist you.\n\n" +
+                "Thank you and have a great day,\n" +
+                "Team DroneDrop";
+
+            // Create a System.Net.Mail.MailAddress object and 
+            // set the sender email address and display name.
+            message.From = new MailAddress("rvider@g.jct.ac.il", "Kedma");
+
+            // Create a System.Net.Mail.SmtpClient object
+            // and set the SMTP host and port number
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+
+            // If your server requires authentication add the below code
+            // =========================================================
+            // Enable Secure Socket Layer (SSL) for connection encryption
+            smtp.EnableSsl = true;
+
+            // Do not send the DefaultCredentials with requests
+            smtp.UseDefaultCredentials = false;
+
+            // Create a System.Net.NetworkCredential object and set
+            // the username and password required by your SMTP account
+            smtp.Credentials = new NetworkCredential("dronedrop2021@gmail.com", "ouiatjczzhkvtxnr");
+            // =========================================================
+
+            // Send the message
+            smtp.Send(message);
+
+            // Redirect to the previous page
+            return RedirectToAction("Index");
+        }
+
         public void SendMailShira(string pdfFilePath)
         {
             try
             {
                 MailMessage mail = new MailMessage();
-                mail.To.Add("ygloberm@g.jct.ac.il");
+                mail.To.Add("rvider@g.jct.ac.il");
                 mail.From = new MailAddress("dronedrop2021@gmail.com");
                 mail.Subject = "Get a new Password";
                 mail.Body = "Hi";
@@ -316,7 +614,7 @@ namespace OnlinePizzaWebApplication.Controllers
             try
             {
                 MailMessage mail = new MailMessage();
-                mail.To.Add("ygloberm@g.jct.ac.il");
+                mail.To.Add("rvider@g.jct.ac.il");
                 mail.From = new MailAddress("dronedrop2021@gmail.com");
                 mail.Subject = "Get a new Password";
                 mail.Body = "Hi ";
@@ -338,54 +636,7 @@ namespace OnlinePizzaWebApplication.Controllers
 }
 
 //}
-//[HttpPost]
-//    public IActionResult SendEmail()
-//    {
-//        // Create a System.Net.Mail.MailMessage object
-//        MailMessage message = new MailMessage();
 
-//        // Add a recipient
-//        message.To.Add("ygloberm@g.jct.ac.il");
-
-//        // Add a message subject
-//        message.Subject = "Account activation";
-
-//        // Add a message body
-//        message.Body = "Hello customer" + "\n" +
-//            "Welcome to Kedma.\n" +
-//            "We are so happy you have chosen to join us  and  are sure you will enjoy our services.\n" +
-//            "For any question, problems or requests you can contact our customer service team " +
-//            "at customerService@DroneDrop.com and they will gladly assist you.\n\n" +
-//            "Thank you and have a great day,\n" +
-//            "Team DroneDrop";
-
-//        // Create a System.Net.Mail.MailAddress object and 
-//        // set the sender email address and display name.
-//        message.From = new MailAddress("yaeligloiberman@gmail.com", "Kedma");
-
-//        // Create a System.Net.Mail.SmtpClient object
-//        // and set the SMTP host and port number
-//        SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-
-//        // If your server requires authentication add the below code
-//        // =========================================================
-//        // Enable Secure Socket Layer (SSL) for connection encryption
-//        smtp.EnableSsl = true;
-
-//        // Do not send the DefaultCredentials with requests
-//        smtp.UseDefaultCredentials = false;
-
-//        // Create a System.Net.NetworkCredential object and set
-//        // the username and password required by your SMTP account
-//        smtp.Credentials = new NetworkCredential("dronedrop2021@gmail.com", "ouiatjczzhkvtxnr");
-//        // =========================================================
-
-//        // Send the message
-//        smtp.Send(message);
-
-//        // Redirect to the previous page
-//        return RedirectToAction("Index");
-//    }
 
 
 
