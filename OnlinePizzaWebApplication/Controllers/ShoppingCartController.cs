0c874e8908e5248913615ddc109cac49e62af033
RivkyShachar <rvider@g.jct.ac.il>
2023-03-19 14:17:29 +0200

    send the cirrect details of the client to email

--- OnlinePizzaWebApplication/Controllers/ShoppingCartController.cs
+++ OnlinePizzaWebApplication/Controllers/ShoppingCartController.cs
@@ -469,6 +469,129 @@ namespace OnlinePizzaWebApplication.Controllers
                 return RedirectToAction("Index");
             }
         }
+
+        [HttpPost]
+        public async Task<IActionResult> GenerateAndSendPdf1(string name, string phone, string email, string notes)
+        {
+            var shoppingCartItems = await _shoppingCart.GetShoppingCartItemsAsync();
+
+            Document Doc = new Document(PageSize.LETTER);
+
+            //Create our file stream
+            using (var memoryStream = new MemoryStream())
+            {
+                // Create a new PDF writer
+                PdfWriter writer = PdfWriter.GetInstance(Doc, memoryStream);
+                // Set the page size to A4
+                Doc.SetPageSize(PageSize.A4);
+                // Set the margins for the document
+                Doc.SetMargins(36f, 36f, 36f, 36f);
+                // Open the PDF document
+                Doc.Open();
+
+                var currentDate = DateTime.Now;
+                // Get the customer name and shipping ID from your data source
+                // Add the date, customer name, and shipping ID to the PDF document
+                var paragraph = new Paragraph($"Date: {currentDate.ToShortDateString()}    Name: {name}    Phone: {phone}    Email: {email}" +
+                    $"Notes: {notes}");
+                paragraph.SpacingAfter = 20f;
+                Doc.Add(paragraph);
+
+                //Full path to the Arial file
+                string ARIALUNI_TFF = System.IO.Path.Combine(@"C:\\Windows\\Fonts", "arial.ttf");
+
+                //Create a base font object making sure to specify IDENTITY-H
+                BaseFont bf = BaseFont.CreateFont(ARIALUNI_TFF, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+
+                //Create a specific font object
+                iTextSharp.text.Font f = new iTextSharp.text.Font(bf, 14);
+
+                //Use a table so that we can set the text direction
+                PdfPTable T = new PdfPTable(5);
+                T.WidthPercentage = 90;
+                T.DefaultCell.BorderWidth = 1;
+                T.RunDirection = PdfWriter.RUN_DIRECTION_RTL;
+                T.AddCell(new Phrase("שם מוצר", f));
+                T.AddCell(new Phrase("ברקוד", f));
+                T.AddCell(new Phrase("כמות בארגז", f));
+                T.AddCell(new Phrase("כמות ביחידים", f));
+                T.AddCell(new Phrase("מחיר", f));
+
+                foreach (var item in shoppingCartItems)
+                {
+                    T.AddCell(new Phrase(item.Pizza.Name, f));
+                    T.AddCell(new Phrase(item.Pizza.Barcode, f));
+                    T.AddCell(new Phrase(item.AmountBoxes.ToString(), f));
+                    T.AddCell(new Phrase(item.AmountSingles.ToString(), f));
+                    T.AddCell(new Phrase(item.Pizza.Price.ToString(), f));
+                }
+                Doc.Add(T);
+
+                if (writer.GetVerticalPosition(false) < Doc.BottomMargin)
+                {
+                    // If the table extends beyond the current page, add a new page
+                    Doc.NewPage();
+                    // Add the date, customer name, and shipping ID to the new page
+                    Doc.Add(paragraph);
+                    // Add the table to the new page
+                    Doc.Add(T);
+                }
+                // Close the PDF document
+                Doc.Close();
+
+
+                var pdfBytes = memoryStream.ToArray();
+
+                // Create a System.Net.Mail.MailMessage object
+                MailMessage message = new MailMessage();
+                //message.From = new MailAddress("rvider@g.jct.ac.il");
+                // Add a recipient
+                message.To.Add("rvider@g.jct.ac.il");
+
+                // Add a message subject
+                message.Subject = "Account activation";
+
+                // Add a message body
+                message.Body = notes;
+
+                // Create a System.Net.Mail.MailAddress object and 
+                // set the sender email address and display name.
+                message.From = new MailAddress("rvider@g.jct.ac.il", "Kedma");
+
+                // Create a MemoryStream object from your PDF file
+                MemoryStream memoryStream1 = new MemoryStream(pdfBytes);
+
+                // Create a new attachment with the PDF bytes
+                Attachment attachment = new Attachment(memoryStream1, "shopping_cart.pdf", "application/pdf");
+
+                // Add the attachment to the email message
+                message.Attachments.Add(attachment);
+
+                // Create a System.Net.Mail.SmtpClient object
+                // and set the SMTP host and port number
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+
+                // If your server requires authentication add the below code
+                // =========================================================
+                // Enable Secure Socket Layer (SSL) for connection encryption
+                smtp.EnableSsl = true;
+
+                // Do not send the DefaultCredentials with requests
+                smtp.UseDefaultCredentials = false;
+
+                // Create a System.Net.NetworkCredential object and set
+                // the username and password required by your SMTP account
+                smtp.Credentials = new NetworkCredential("dronedrop2021@gmail.com", "ouiatjczzhkvtxnr");
+                // =========================================================
+
+                // Send the message
+                smtp.Send(message);
+
+
+                // Redirect to the previous page
+                return RedirectToAction("Save");
+            }
+        }
     }
 }
 
